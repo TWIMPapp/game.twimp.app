7efc043f956cab289ef7841f2ea1d78028c1ed71
Craig Thomas <craig@tinybigideas.com>
2023-06-06 17:59:33 +0100

    add heads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" className="h-full w-full">
-      <Head />
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+        <meta name="referrer" content="origin-when-cross-origin"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        ></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
