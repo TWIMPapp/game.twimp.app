042720ee1e15fe9b8b47c8d807291a006a5b89eb
Craig Thomas <craig@tinybigideas.com>
2023-07-06 00:45:07 +0100

    no longer default for upside down

--- src/pages/t/chat.tsx
+++ src/pages/t/chat.tsx
@@ -159,7 +159,7 @@ export default function Multi() {
           energy={energy}
           isTyping={isTyping}
           theme={params?.theme}
-          upsideDown={params?.upside_down ?? true}
+          upsideDown={params?.upside_down}
           callback={messageCallback}
         />
       ) : (
