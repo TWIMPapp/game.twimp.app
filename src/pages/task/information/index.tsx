7c9e8c2db498c5ac06bf263f9a38cd4e1a5c3468
Craig Thomas <craig@tinybigideas.com>
2024-01-02 18:36:18 +0000

    make info button outlined

--- src/pages/task/information/index.tsx
+++ src/pages/task/information/index.tsx
@@ -67,7 +67,7 @@ export default function Information() {
         </div>
       )}
       <div className="flex justify-end p-4 pb-8">
-        <Button className="px-4 py-2" onClick={goToNextTask}>
+        <Button className="px-4 py-2" onClick={goToNextTask} variant="contained">
           Next
         </Button>
       </div>
