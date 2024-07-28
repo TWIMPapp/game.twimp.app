bea6d43f07cf9c1281e31c7d17bf1335b24fb9ec
Craig Thomas <craig@tinybigideas.com>
2024-07-28 11:37:05 +0100

    make full screen

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"></meta>
         <meta name="referrer" content="same-origin"></meta>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="mobile-web-app-capable" content="yes"></meta>
       </Head>
       <body>
         <Main />
