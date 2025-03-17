08d0d9b036759d86dd85bc8e1b90abe92eeca6d5
Craig Thomas <craig@tinybigideas.com>
2025-03-17 13:02:48 +0000

    remove view port from head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en" className="h-full w-full">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"></meta>
         <meta name="referrer" content="same-origin"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
